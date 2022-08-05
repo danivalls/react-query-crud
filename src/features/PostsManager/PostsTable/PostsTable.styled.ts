28debb7b47587cb414613ae2167a44f57d9aa446
Daniel Valls <daniel.valls.mestre@gmail.com>
2022-08-05 17:14:48 +0200

    Fix table header styles

--- src/features/PostsManager/PostsTable/PostsTable.styled.ts
+++ src/features/PostsManager/PostsTable/PostsTable.styled.ts
@@ -4,7 +4,8 @@ import styled from "styled-components";
 export const TableCellHeader = styled(TableCell)`
   background-color: ${({ theme }) => theme.colors.contentBackground};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
-  line-height: 0.5rem;
+  line-height: 1rem;
+  padding: 0.75rem;
   white-space: nowrap;
 `;
 
