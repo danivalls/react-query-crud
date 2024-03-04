04481ea5e0dc9a1278188a1be6d1503b1ece8ebe
Daniel Valls <daniel.valls.mestre@gmail.com>
2024-03-04 18:07:15 +0100

    Add React Query Devtools and homepage URL

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -3,6 +3,7 @@ import {
   createTheme,
 } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React, { FC, PropsWithChildren } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, theme } from "theme";
@@ -21,6 +22,7 @@ export const Providers: FC<PropsWithChildren> = ({ children }) => {
       <MUIThemeProvider theme={muiTheme}>
         <ThemeProvider theme={theme}>
           <GlobalStyles />
+          <ReactQueryDevtools initialIsOpen={false} />
           {children}
         </ThemeProvider>
       </MUIThemeProvider>
